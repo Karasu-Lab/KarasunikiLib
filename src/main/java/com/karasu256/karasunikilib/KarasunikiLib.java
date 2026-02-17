903771f8a790689a813b5d5c6fb5290808ac363b
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-02-18 02:40:43 +0900

    fix: fixed library config

---
+++ src/main/java/com/karasu256/karasunikilib/KarasunikiLib.java
@@ -0,0 +1,10 @@
+package com.karasu256.karasunikilib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KarasunikiLib implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
