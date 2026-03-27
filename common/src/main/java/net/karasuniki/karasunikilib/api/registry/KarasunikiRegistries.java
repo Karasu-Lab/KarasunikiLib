71234e26def1e401a877e1bc47885c045196cec7
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-03-28 01:39:37 +0900

    fix: fixed registry error

--- common/src/main/java/net/karasuniki/karasunikilib/api/registry/KarasunikiRegistries.java
+++ common/src/main/java/net/karasuniki/karasunikilib/api/registry/KarasunikiRegistries.java
@@ -1,5 +1,6 @@
 package net.karasuniki.karasunikilib.api.registry;
 
+import dev.architectury.registry.registries.RegistrarManager;
 import net.karasuniki.karasunikilib.api.KarasunikiLib;
 import net.karasuniki.karasunikilib.api.data.IEnergy;
 import net.minecraft.core.Registry;
@@ -10,5 +11,6 @@ public class KarasunikiRegistries {
     public static final ResourceKey<Registry<IEnergy>> ENERGY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(KarasunikiLib.MOD_ID, "energy"));
 
     public static void init() {
+        RegistrarManager.get(KarasunikiLib.MOD_ID).<IEnergy>builder(ENERGY.location()).build();
     }
 }
