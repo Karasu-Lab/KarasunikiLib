813eda292aeab04daef05b66e737aeb0273a8c2a
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-03-27 17:42:27 +0900

    fix: fixed format

--- common/src/main/java/net/karasuniki/karasunikilib/bridge/geckolib/IGeoModelProvider.java
+++ common/src/main/java/net/karasuniki/karasunikilib/bridge/geckolib/IGeoModelProvider.java
@@ -4,6 +4,8 @@ import net.minecraft.resources.ResourceLocation;
 
 public interface IGeoModelProvider {
     ResourceLocation getModelResource();
+
     ResourceLocation getTextureResource();
+
     ResourceLocation getAnimationResource();
 }
