17dfdc78714e33062623ece0c8176d39e1d20aae
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-03-28 01:40:19 +0900

    feat: simplified id uses

--- common/src/main/java/net/karasuniki/karasunikilib/api/registry/KarasunikiRegistries.java
+++ common/src/main/java/net/karasuniki/karasunikilib/api/registry/KarasunikiRegistries.java
@@ -3,12 +3,13 @@ package net.karasuniki.karasunikilib.api.registry;
 import dev.architectury.registry.registries.RegistrarManager;
 import net.karasuniki.karasunikilib.api.KarasunikiLib;
 import net.karasuniki.karasunikilib.api.data.IEnergy;
+import net.karasuniki.karasunikilib.utils.Id;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 
 public class KarasunikiRegistries {
-    public static final ResourceKey<Registry<IEnergy>> ENERGY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(KarasunikiLib.MOD_ID, "energy"));
+    public static final ResourceKey<Registry<IEnergy>> ENERGY = ResourceKey.createRegistryKey(Id.id("energy"));
 
     public static void init() {
         RegistrarManager.get(KarasunikiLib.MOD_ID).<IEnergy>builder(ENERGY.location()).build();
