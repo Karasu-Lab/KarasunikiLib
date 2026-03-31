551f36e8919726f2b58d7156c59d9361bb56b907
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-04-01 01:21:01 +0900

    delete: deleted geckolib interfaces

--- common/src/main/java/net/karasuniki/karasunikilib/bridge/geckolib/GeckoLibHelper.java
+++
@@ -1,8 +0,0 @@
-package net.karasuniki.karasunikilib.bridge.geckolib;
-
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-
-public interface GeckoLibHelper {
-    <T extends BlockEntity> void registerBlockRenderer(BlockEntityType<T> type);
-}
--- fabric/src/main/java/net/karasuniki/karasunikilib/fabric/client/KarasunikiLibFabricClient.java
+++ fabric/src/main/java/net/karasuniki/karasunikilib/fabric/client/KarasunikiLibFabricClient.java
@@ -1,11 +1,9 @@
 package net.karasuniki.karasunikilib.fabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.karasuniki.karasunikilib.api.client.registry.KClientScanner;
 
 public final class KarasunikiLibFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        KClientScanner.scanAll();
     }
 }
