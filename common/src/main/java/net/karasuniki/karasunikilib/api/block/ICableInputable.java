28c009c2855a8e12becd06a34bfc1ffaca6a0c9d
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2026-04-02 18:38:44 +0900

    feat: added default methods

--- common/src/main/java/net/karasuniki/karasunikilib/api/block/ICableInputable.java
+++ common/src/main/java/net/karasuniki/karasunikilib/api/block/ICableInputable.java
@@ -1,7 +1,13 @@
 package net.karasuniki.karasunikilib.api.block;
 
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 
 public interface ICableInputable {
     long insert(ResourceLocation id, long maxAmount, boolean simulate);
+
+    default long insert(ResourceLocation id, long maxAmount, boolean simulate, @Nullable Direction side) {
+        return insert(id, maxAmount, simulate);
+    }
 }
--- common/src/main/java/net/karasuniki/karasunikilib/api/block/ICableOutputable.java
+++ common/src/main/java/net/karasuniki/karasunikilib/api/block/ICableOutputable.java
@@ -1,7 +1,13 @@
 package net.karasuniki.karasunikilib.api.block;
 
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 
 public interface ICableOutputable {
     long extract(ResourceLocation id, long maxAmount, boolean simulate);
+
+    default long extract(ResourceLocation id, long maxAmount, boolean simulate, @Nullable Direction side) {
+        return extract(id, maxAmount, simulate);
+    }
 }
